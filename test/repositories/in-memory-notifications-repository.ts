ee1e99a563c884d0c5169cf4225ec8e8f3f04671
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-15 14:08:33 -0300

    feat: count recipient notification

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -11,6 +11,12 @@ export class inMemoryNotificationsRepository
     return !notification ? null : notification;
   }
 
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
